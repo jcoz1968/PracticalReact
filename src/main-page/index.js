622b858ce406aaaaf5efd46288544c135e4690ae
Jeffery Cosby <jeffery.cosby@omes.ok.gov>
2021-11-21 12:55:21 -0600

    fetch houses from json file

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,12 +1,21 @@
+import { useEffect } from 'react';
 import './main-page.css';
-import Header from './header'
+import Header from './header';
 
 function App() {
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const res = await fetch('/houses.json');
+      const houses = await res.json();
+    };
+    fetchHouses();
+  }, []);
   return (
     <div className="container">
-      <Header 
-        subtitle='Providing houses all over the world' 
-        title='Some title' />
+      <Header
+        subtitle="Providing houses all over the world"
+        title="Some title"
+      />
     </div>
   );
 }
